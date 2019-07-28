3c4e65565ca73718f15d01533eb480e29288b191
Shashank <shashankgpt270@gmail.com>
2019-07-28 19:01:32 +0530

    shared state

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,6 +5,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Store, select } from '@ngrx/store';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as fromAuth from '../../shared/state/shared.reducer';
+import { ISnackbar } from 'src/app/user/dataTypes';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -32,6 +33,11 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   onSubmit() {
+    this.store.dispatch({
+      type: 'SPINNER_ACTIVATE',
+      payload: true
+    });
+
     this.authService.login(this.f.username.value, this.f.password.value).subscribe(resData => {
       const loginData = resData.data;
       this.store.dispatch({
@@ -47,6 +53,19 @@ export class LoginComponent implements OnInit {
         authentications => {
           localStorage.setItem('login', authentications.tokenCodeValue);
           this.openSnackBar(resData.Message, 'Login');
+          const snack1: ISnackbar = {
+            snackBarActive: true,
+            snackBarMessage: resData.Message,
+            snackBarAction: 'Login'
+          };
+          this.store.dispatch({
+            type: 'SET_NOTIFY',
+            payload: snack1
+          });
+          this.store.dispatch({
+            type: 'SPINNER_ACTIVATE',
+            payload: false
+          });
           this.router.navigate(['/user/view']);
         }
       );
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,26 +4,31 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as fromShared from '../shared/state/shared.reducer';
 import { Store, select } from '@ngrx/store';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor(private breakpointObserver: BreakpointObserver, private store: Store<fromShared.State>) {}
+  spinner = false;
+  constructor(private breakpointObserver: BreakpointObserver, private snackBar: MatSnackBar, private store: Store<fromShared.State>) {}
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
+    openSnackBar(msg, action) {
+      this.snackBar.open(msg, action, {
+        duration: 2000,
+      });
+    }
   ngOnInit(): void {
     this.store.pipe(select('shared')).subscribe((data: fromShared.State) => {
-      alert(data.loggedIn);
-      console.log("main data");
-      console.log(data);
-      console.log("main data end");
+      if(data.snackBar.snackBarActive) {
+      this.spinner = data.spinnerActive;
+      this.openSnackBar(data.snackBar.snackBarMessage, data.snackBar.snackBarAction);
+      }
     });
   }
 
