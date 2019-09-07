bb8cc60000860fd05a8105e543f4cd52b32569f5
Shashank <shashankgpt270@gmail.com>
2019-09-07 15:20:53 +0530

    component test

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from '../../core/core.module';
+import { SharedModule } from '../../shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from '../state/authenticate.reducer';
+import { AuthEffects } from '../state/auth.effects';
+import { EffectsModule } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +18,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [CoreModule, SharedModule,RouterTestingModule,NoopAnimationsModule,
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([]),
+        StoreModule.forFeature('authentications', reducer),
+        EffectsModule.forFeature([AuthEffects])],
     })
     .compileComponents();
   }));
@@ -21,5 +36,11 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnDestroy', () => {
+    component.componentActive = true;
+    component.ngOnDestroy();
+    expect(component.componentActive).toBeFalsy();
   });
 });
