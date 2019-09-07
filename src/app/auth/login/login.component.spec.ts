80a6c187eb2347e0d04b7fa41ba9ece1ecc918f2
Shashank <shashankgpt270@gmail.com>
2019-09-07 16:15:19 +0530

    login component unit and integrated test

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -4,38 +4,23 @@ import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 import { reducer } from '../state/authenticate.reducer';
 import { AuthEffects } from '../state/auth.effects';
 import { EffectsModule } from '@ngrx/effects';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import * as fromAuth from '../state';
+import * as fromShared from '../../shared/state';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
-      imports: [CoreModule, SharedModule,RouterTestingModule,NoopAnimationsModule,
-        StoreModule.forRoot({}),
-        EffectsModule.forRoot([]),
-        StoreModule.forFeature('authentications', reducer),
-        EffectsModule.forFeature([AuthEffects])],
-    })
-    .compileComponents();
-  }));
+  let store: Store<fromAuth.State>;
+  let shareStore: Store<fromShared.State>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    component = new LoginComponent(store, shareStore);
   });
 
   it('ngOnDestroy', () => {
